52d4a54cd11af82716320efaa43025d519423a16
Bhaskar Neupane <60285479+vaskr-neup@users.noreply.github.com>
2025-07-26 17:26:46 -0400

    test

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          {/* Test code */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
